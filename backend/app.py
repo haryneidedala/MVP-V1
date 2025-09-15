cca219787b820da67c00f4b1ed77ae105ef08709
haryneidedala <hfloriano@icloud.com>
2025-09-15 16:13:50 +0200

    new Directories with empty files for preparation

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,32 @@
-from flask import request, jsonify
-from models import db, Workout
+from flask import request, jsonify, Flask
+from datetime import datetime
+## from models import db, Workout
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///profiles.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Workout(db.Model): # <- Sagt: "Erstelle eine Workout-Tabelle"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False) ## <- Wie eine Excel-Spalte für Text
+    description = db.Column(db.Text) # <- eine Spalte für Beschreibung
+    duration = db.Column(db.Integer) ## <- Eine Spalte für Zahlen
+    difficulty = db.Column(db.Enum('Anfänger', 'Fortgeschritten', 'Profi'))
+    # created_by = db.Column(db.Integer, db.ForeignKey('user.id'))  # Falls User-Tabelle existiert
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/workouts', methods=['GET'])
 def get_workouts():
@@ -22,3 +49,6 @@ def create_workout():
     db.session.add(workout)     # <- Fügt sie zur Datenbank hinzu
     db.session.commit()
     return jsonify({'message': 'Workout created!'}), 201
+
+if __name__ == "__main__":
+    app.run(port=os.getenv("PORT"), debug=True, host="0.0.0.0")
